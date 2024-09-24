97c7135e6c16fc2954ad92e10759efbcac34812d
olesm88 <olesmakarenko@gmail.com>
2024-09-23 20:12:55 -0500

    first commit

---
+++ src/test/java/ReversedString.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReversedString {
+    @Test
+    public void reverseTestWithNull(){
+        Assert.assertNull(ReverseString.reverseString(null));
+        System.out.println("reverseTestWithNull");
+    }
+
+    @Test
+    public void reverseTestWithDigits(){
+        Assert.assertEquals("cbA321",ReverseString.reverseString("123Abc"));
+        System.out.println("reverseTestWithDigits");
+    }
+
+    @Test
+    public void reverseTestHappyPAth(){
+        Assert.assertEquals("elppa",ReverseString.reverseString("apple"));
+        System.out.println("reverseTestHappyPAth");
+        System.out.println("reverseTestHappyPAth added");
+
+    }
+
+
+}
